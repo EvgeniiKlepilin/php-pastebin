f2ad943c36d7e2e2153148ff8167bb0184101aee
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2016-11-14 08:31:56 +0900

    Update connect.php
    Final change to connect

--- connect.php
+++ connect.php
@@ -1,19 +1,26 @@
 <?php
 class Database{
+  private static $connection = NULL;
+  
+  public function getConnection(){
+    //credentials - can be stored in a separate file and then included
+    $host = "localhost";
+    $username = "root";
+    $password = "pAzzW0rd";
+    $dbname = "pastebin";
 
-  //credentials - can be stored in a separate file and then included
-  $host = "localhost";
-  $username = "root";
-  $password = "pAzzW0rd";
-  $dbname = "pastebin";
-
-  //connect to database
-  try{
-    $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  } catch(PDOException $error){
-    echo $error->getMessage();
+    //connect to database
+    try{
+      if(!isset($connection)){
+        $this->connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      return $this->connection;
+    } catch(PDOException $error){
+      echo $error->getMessage();
+    }
   }
+
+  
 }
 ?>
