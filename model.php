ceabf8de25ca6e5e9d8584a1068b69232cad0785
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2016-11-14 08:23:08 +0900

    Update model.php

--- model.php
+++ model.php
@@ -1,3 +1,16 @@
 <?php
-//model.php
+//credentials - can be stored in a separate file and then included
+$host = "localhost";
+$username = "root";
+$password = "pAzzW0rd";
+$dbname = "pastebin";
+
+//connect to database
+try{
+  $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+} catch(PDOException $error){
+  echo $error->getMessage();
+}
 ?>
