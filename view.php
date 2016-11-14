b06dc8532014cef5ffaa1d00f5cc337874d81bb7
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2016-11-14 09:13:15 +0900

    Update view.php

--- view.php
+++ view.php
@@ -1,3 +1,17 @@
 <?php
 //view.php
+class pastebinView{
+  private $model;
+  private $controller;
+  
+  public function __construct($model, $controller){
+    $this->model = $model;
+    $this->controller = $controller;
+  }
+  
+  public function showNotes(){
+    foreach($this->model->savedNotes as $note){
+      echo "<p>" . $note . "</p>";
+  }
+}
 ?>
