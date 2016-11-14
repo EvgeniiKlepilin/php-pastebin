440f3a2bcb0cb8f517892c0ed4cfcb8400b54ab8
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2016-11-14 09:57:00 +0900

    Added html output
    HTML output. Will need to call this function from index.php

--- view.php
+++ view.php
@@ -13,5 +13,22 @@ class pastebinView{
     foreach($this->model->savedNotes as $note){
       echo "<p>" . $note . "</p>";
   }
+    
+  public function generateHTML(){
+    echo "<html>
+            <head>
+              <title>PasteBin Clone</title>
+            </head>
+            <body>
+              <h1 style='text-align:center'>Notes</h1>" . $this.showNotes() .
+            "<hr>
+            <form action=<?php echo htmlentities($_SERVER['PHP_SELF']?> method=post>
+              <input type='submit' />
+            </form>
+            Note:<textarea id='noteArea' rows='6' cols='75' />
+            <hr>
+            </body>
+            </html>";
+  }
 }
 ?>
