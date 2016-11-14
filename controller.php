c7dfac80e007f1ade2d694c099d746c7f631c41b
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2016-11-14 09:25:14 +0900

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,3 +1,17 @@
 <?php
 //controller.php
+class pasteBinControl{
+  private $model;
+  
+  public function __construct($model){
+    $this->model = $model;
+  }
+  
+  public function updatePastebin($conn, $newNote){
+    //insert new note into db
+    $sql = $conn->prepare("INSERT INTO pastebin (note) VALUES (:newNote)";
+    $sql->bindParam(:newNote, $newNote);
+    $sql->execute();
+  }
+}
 ?>
