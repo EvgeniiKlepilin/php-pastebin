e58db59c8603efb5ee3c43ee4ad1b7d71597259b
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2016-11-14 10:18:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 //index.php
+
+//Creation and initialization of objects
+//initialize database connection
+$connection = new Database();
+$connection.getConnection();
+//initialize objects of MVC
+$pastebin = new pastebinModel();
+$control = new pastebinControl($pastebin);
+$view = new pastebinView($pastebin, $control);
+
+if(isset($_POST['noteArea']) && !empty($_POST['noteArea'])){
+  $control.updatePastebin($connection, $_POST['noteArea']);
+  $view.generateHTML();
+} else {
+  $view.generateHTML();
+}
 ?>
