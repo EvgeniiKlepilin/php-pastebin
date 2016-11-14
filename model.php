8b3492cf06570fb8a487e300c43b11c6a718e690
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2016-11-14 09:34:44 +0900

    Update model.php

--- model.php
+++ model.php
@@ -3,6 +3,17 @@
 class pasteBinModel{
   public $savedNotes = array();
   
+  public function updateNotes($conn){
+    //select all notes from db
+    $sql = $conn->query("SELECT note FROM pastebin");
+    //update local array
+    if($sql->num_rows > 0){
+      while($row = $sql->fetch_assoc()){
+        array_push($savedNotes, $row['note']);
+      }
+    }
+    
+  }
   
 }
 ?>
