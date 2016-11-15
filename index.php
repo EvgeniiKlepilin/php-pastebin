cc81791b312e590ec9d24d540a11553e45f5c889
EvgeniiKlepilin <evgenii.klepilin@gmail.com>
2016-11-15 21:58:35 +0900

    Update index.php
    -> instead of .

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 //Creation and initialization of objects
 //initialize database connection
 $connection = new Database();
-$connection.getConnection();
+$connection->getConnection();
 //initialize objects of MVC
 $pastebin = new pastebinModel();
 $control = new pastebinControl($pastebin);
 $view = new pastebinView($pastebin, $control);
 
 if(isset($_POST['noteArea']) && !empty($_POST['noteArea'])){
-  $control.updatePastebin($connection, $_POST['noteArea']);
-  $view.generateHTML();
+  $control->updatePastebin($connection, $_POST['noteArea']);
+  $view->generateHTML();
 } else {
-  $view.generateHTML();
+  $view->generateHTML();
 }
 ?>
